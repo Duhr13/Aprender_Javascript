b88789cd8a9436c8024f361fcde260977eaa3f89
Duhr13 <165281590+Duhr13@users.noreply.github.com>
2024-03-31 14:37:50 +0200

    Add files via upload

---
+++ Programas con matrices/todos_los_elementos_de_una_matriz/elementos_de_una_matriz.js
@@ -0,0 +1,7 @@
+'use strict'
+
+let matriz = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"];
+
+for (let i = 0; i < matriz.length; i++){
+    document.write("El elemento número " + i + " de la matriz es: " + matriz[i] + "<br>");
+}
