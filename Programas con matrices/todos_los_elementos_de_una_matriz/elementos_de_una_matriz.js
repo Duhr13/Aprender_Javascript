a9c5ce29c81cfa8b2a2caefdb0636401ce10113c
Duhr13 <165281590+Duhr13@users.noreply.github.com>
2024-04-03 14:03:39 +0200

    Add files via upload

--- Programas con matrices/todos_los_elementos_de_una_matriz/elementos_de_una_matriz.js
+++ Programas con matrices/todos_los_elementos_de_una_matriz/elementos_de_una_matriz.js
@@ -2,6 +2,24 @@
 
 let matriz = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"];
 
+/*
+    Forma burda y normal de recorrer una matriz usando un bucle for.
+*/
+
 for (let i = 0; i < matriz.length; i++){
     document.write("El elemento número " + i + " de la matriz es: " + matriz[i] + "<br>");
 }
+
+document.write('<br>');
+document.write('<br>');
+document.write('-----------------------------------------------------------------------------------------------------')
+document.write('<br>');
+document.write('<br>');
+
+/*
+    Forma usando un bucle for each
+*/
+
+matriz.forEach((element, index) => {
+    document.write("El elemento número " + index + " de la matriz es: " + element + "<br>");
+});
