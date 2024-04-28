f6f3f41d165b17c7d1fdadd1a0c3fe935b7a8922
Duhr13 <165281590+Duhr13@users.noreply.github.com>
2024-04-28 13:47:48 +0200

    Add files via upload
    He añadido el bucle for in y he jugado con .map()

--- Programas con matrices/todos_los_elementos_de_una_matriz/elementos_de_una_matriz.js
+++ Programas con matrices/todos_los_elementos_de_una_matriz/elementos_de_una_matriz.js
@@ -12,7 +12,7 @@ for (let i = 0; i < matriz.length; i++){
 
 document.write('<br>');
 document.write('<br>');
-document.write('-----------------------------------------------------------------------------------------------------')
+document.write('<hr>>')
 document.write('<br>');
 document.write('<br>');
 
@@ -23,3 +23,31 @@ document.write('<br>');
 matriz.forEach((element, index) => {
     document.write("El elemento número " + index + " de la matriz es: " + element + "<br>");
 });
+
+document.write('<br>');
+document.write('<br>');
+document.write('<hr>>')
+document.write('<br>');
+document.write('<br>');
+
+/*
+    Forma usando un bucle for in
+*/
+
+for (let index in matriz) {
+    document.write("El elemento número " + index + " de la matriz es: " + matriz[index] + "<br>");
+}
+
+/*
+    Modificando el array/matriz utilizando map() para sacar un string modificado
+*/
+
+document.write('<br>');
+document.write('<br>');
+document.write('<hr>>')
+document.write('<br>');
+document.write('<br>');
+
+let nuevaMatriz = matriz.map(function(element, index){
+    return document.write("El elemento número " + index + " de la matriz es: " + element + " modificado.<br>");
+})
