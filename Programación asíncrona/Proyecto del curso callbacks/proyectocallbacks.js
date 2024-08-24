2521e5d6d204dad5545ee3b7e0ce75bc259ffd11
Duhr13 <165281590+Duhr13@users.noreply.github.com>
2024-08-24 17:01:24 +0200

    .

--- Programación asíncrona/Proyecto del curso callbacks/proyectocallbacks.js
+++ Programación asíncrona/Proyecto del curso callbacks/proyectocallbacks.js
@@ -5,7 +5,24 @@
 function cargarContenido(){
     // Función que cargue las cotizaciones
 
+    cargarCotizaciones(mostrarCotizacion);
+
     // Función que cargue los elementos
 
     // Función que cargue los textos
+}
+
+async function cargarCotizaciones(callback) {
+
+    let promesa1 = await fetch('https://api.coindesk.com/v1/bpi/currentprice.json'); // Esta promesa se hace con callback
+    callback(await promesa1.json());
+
+    let promesa2 = await fetch('https://open.er-api.com/v6/latest/USD');
+    let datos2 = await promesa2.json();
+    document.getElementById('UsdEur').append(datos2.rates.EUR); // A diferencia del callback de promesa1, el dato está en 'rates'
+
+}
+
+function mostrarCotizacion(datos) {
+    document.getElementById('BitcoinUsd').append(datos.bpi.USD.rate); // El dato buscado en el .json está en 'rate'
 }
