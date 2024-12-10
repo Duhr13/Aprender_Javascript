22ff111a95b973ff60f77bd13977c4e729ebf04b
Duhr13 <165281590+Duhr13@users.noreply.github.com>
2024-12-10 13:55:07 +0100

    Add files via upload

---
+++ Cajon deSastre/mis_pruebas v0001/index.js
@@ -0,0 +1,89 @@
+'use strict'
+
+// VARIABLES GLOBALES DE RESULTADO
+
+let res1 = document.getElementById("resul1");
+let res2 = document.getElementById("resul2");
+let res3 = document.getElementById("resul3");
+let res4 = document.getElementById("resul4");
+let res5 = document.getElementById("resul5");
+let res6 = document.getElementById("resul6");
+let res7 = document.getElementById("resul7");
+let res8 = document.getElementById("resul8");
+let res9 = document.getElementById("resul9");
+let res10 = document.getElementById("resul10");
+let res11 = document.getElementById("resul11");
+
+// VARIABLES GLOBALES DEL PANEL
+
+let d1 = document.getElementById("D1"); 
+let d2 = document.getElementById("D2");
+let d3 = document.getElementById("D3");
+let d4 = document.getElementById("D4");
+let d5 = document.getElementById("D5");
+let d6 = document.getElementById("D6");
+let d7 = document.getElementById("D7");
+let d8 = document.getElementById("D8");
+let d9 = document.getElementById("D9");
+let d10 = document.getElementById("D10");
+let d11 = document.getElementById("D11");
+
+let C1 = document.getElementById("C1"); 
+let C2 = document.getElementById("C2");
+let C3 = document.getElementById("C3");
+let C4 = document.getElementById("C4");
+let C5 = document.getElementById("C5");
+let C6 = document.getElementById("C6");
+let C7 = document.getElementById("C7");
+let C8 = document.getElementById("C8");
+let C9 = document.getElementById("C9");
+let C10 = document.getElementById("C10");
+let C11 = document.getElementById("C11");
+
+let cc1 = document.getElementById("cc1"); 
+let cc2 = document.getElementById("cc2");
+let cc3 = document.getElementById("cc3");
+let cc4 = document.getElementById("cc4");
+let cc5 = document.getElementById("cc5");
+let cc6 = document.getElementById("cc6");
+let cc7 = document.getElementById("cc7");
+let cc8 = document.getElementById("cc8");
+let cc9 = document.getElementById("cc9");
+let cc10 = document.getElementById("cc10");
+let cc11 = document.getElementById("cc11");
+
+let E1 = document.getElementById("E1"); 
+let E2 = document.getElementById("E2");
+let E3 = document.getElementById("E3");
+let E4 = document.getElementById("E4");
+let E5 = document.getElementById("E5");
+let E6 = document.getElementById("E6");
+let E7 = document.getElementById("E7");
+let E8 = document.getElementById("E8");
+let E9 = document.getElementById("E9");
+let E10 = document.getElementById("E10");
+let E11 = document.getElementById("E11");
+
+let ee1 = document.getElementById("ee1"); 
+let ee2 = document.getElementById("ee2");
+let ee3 = document.getElementById("ee3");
+let ee4 = document.getElementById("ee4");
+let ee5 = document.getElementById("ee5");
+let ee6 = document.getElementById("ee6");
+let ee7 = document.getElementById("ee7");
+let ee8 = document.getElementById("ee8");
+let ee9 = document.getElementById("ee9");
+let ee10 = document.getElementById("ee10");
+let ee11 = document.getElementById("ee11");
+
+let f1 = document.getElementById("f1"); 
+let f2 = document.getElementById("ee2");
+let f3 = document.getElementById("ee3");
+let f4 = document.getElementById("ee4");
+let f5 = document.getElementById("ee5");
+let f6 = document.getElementById("ee6");
+let f7 = document.getElementById("ee7");
+let f8 = document.getElementById("ee8");
+let f9 = document.getElementById("ee9");
+let f10 = document.getElementById("ee10");
+let f11 = document.getElementById("ee11");
