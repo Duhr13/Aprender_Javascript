d5c097122e6f3f1870968e4e66aaec160946cbf6
Duhr13 <165281590+Duhr13@users.noreply.github.com>
2025-04-18 11:14:36 +0200

    .

--- Node/02 - Bases/src/app.js
+++ Node/02 - Bases/src/app.js
@@ -1,6 +1,21 @@
-const {emailTemplate} = require('./js-foundation/01-template.js');
-require('./js-foundation/02-destructuring.js');
+//const {emailTemplate} = require('./js-foundation/01-template.js');
+//require('./js-foundation/02-destructuring.js');
+const {getUserById} = require('./js-foundation/03-callbacks.js');
 
+/* console.log(emailTemplate);
 
+const characters = ['Flash', 'Superman', 'Green Lantern', 'Batman'];
 
-// console.log(emailTemplate);
+const [ , , , batman] = characters;
+
+console.log(batman); */
+
+const id = 3;
+
+getUserById(id, function (error, user){
+    if(error) {
+        throw new Error(error);
+    }
+
+    console.log(user);
+});
